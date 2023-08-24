9fa00f138cf46f36625e725eeab9bf150180d0f4
Ashuraj-Herode22 <143123237+Ashuraj-Herode22@users.noreply.github.com>
2023-08-25 00:46:33 +0530

    Adding react native files

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native'
+
+const App =()=>{
+  return(
+    <View> 
+      <Text style={{fontSize:40}}> Hello React native </Text> 
+    </View>
+  )
+}
+
+export default App;
